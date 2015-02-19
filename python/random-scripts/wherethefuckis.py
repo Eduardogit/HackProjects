051da19965303387e5d7de0c179948fc8f11cfc5
eduardogit <itenriquez_isc@hotmail.com>
2015-02-19 15:16:27 -0600

    Adding color.py and finished th first 3 menus

--- python/random-scripts/wherethefuckis.py
+++ python/random-scripts/wherethefuckis.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import color
 import os
 from time import sleep
 import subprocess as sub
 os.system('clear')
-menu = [
+images   = [".gif",".png",".jpg",".jpeg",".svg"]
+document = [".doc",".pdf",".xls",".xml",".txt"]
+code     = [".html",".js",".sh",".c",".h",".py",".rb",".java",".class"]
+audio    = [".mp4",".mp3",".wav",".wma",".ogg"]
+menu     = [
 		"\t====================[ FSS ] ===================\n"
 		"\t=== [F] i l e [S] e a r c h [S] c a n n e r ===\n"
 		"\t===============================================\n"
 		"\t      [       Select the target          ]\n\n\n"
 		"[1] FILES     					      \n"
 		"[2] FOLDERS 						  \n"
-		"[3] SPECIFIC FILE 				      \n"
+		"[3] SPECIFIC FILES 				      \n"
 		"[4] SPECIFIC WORD 					  \n"
 		"[5] EXTRAS PASSWORDS,LOGS,ETC 		  \n"
 	   ]
@@ -23,12 +30,14 @@ def FILES(phrase):
 			for filerow in files:
 				filerow = filerow.lower()
 				if phrase in filerow:
-					print "[+]FILE FOUND [%s]=>[%s]"%(filerow,base)
+					color.infog("[+]FILE FOUND [%s]=>[%s]"%(filerow,base))
 					TotalFiles += 1
 		print "TOTAL FILES FOUND [ %i ]"%TotalFiles
 	except Exception, e:
 		raise e
-	
+###############
+### xdg-open [URL]
+###############
 def FOLDERS(phrase):
 	try:
 		phrase = phrase.lower()
@@ -36,15 +45,52 @@ def FOLDERS(phrase):
 			dirs = map(lambda x: x.lower(), dirs)
 			for singledir in dirs:
 				if phrase == singledir:
-					print "[+]FILE FOUND [%s]=>[%s]"%(singledir,base)
-			sleep(2)
+					color.infog("[+]FILE FOUND [%s]=>[%s]"%(singledir,base))
 	except Exception, e:
 		raise e
 	
 
-def SPECIFICFILE(ext,phrase):
-	for base, dirs, files in os.walk('/'):
-		print files
+def SPECIFICFILES():
+	print "CHOOSE FILE TYPE"
+	file_type = raw_input("\n[1] images\n[2] document\n[3] code\n[4] audio\n\n\n>:")
+	########################### [ IMAGES  ] ###########################
+	if file_type   == "1":
+		for number,i in enumerate(images):
+			print "[%i] %s"%(number+1,i)
+		choose = input("\n>:")
+		for base,dirs,files in os.walk('/home/desktopadmin/Imágenes'):
+			for single in files:
+				if single.endswith(images[choose-1]):
+					color.infog( "FILE FOUND[%s]=>[%s]"%(single,base))
+	########################### [ DOCUMENTS  ] ###########################
+	elif file_type == "2":
+		for number,i in enumerate(document):
+			print "[%i] %s"%(number+1,i)
+		choose = input("\n>:")
+		for base,dirs,files in os.walk('/'):
+			for single in files:
+				if single.endswith(document[choose-1]):
+					color.infog( "FILE FOUND[%s]=>[%s]"%(single,base))
+	########################### [ CODE  ] ###########################
+	elif file_type == "3":
+		for number,i in enumerate(code):
+			print "[%i] %s"%(number+1,i)
+		choose = input("\n>:")
+		for base,dirs,files in os.walk('/'):
+			for single in files:
+				if single.endswith(code[choose-1]):
+					color.infog( "FILE FOUND[%s]=>[%s]"%(single,base))
+	########################### [ AUDIO  ] ###########################
+	elif file_type == "4":
+		for number,i in enumerate(audio):
+			print "[%i] %s"%(number+1,i)
+		choose = input("\n>:")
+		for base,dirs,files in os.walk('/'):
+			for single in files:
+				if singlengle.endswith(audio[choose-1]):
+					color.infog( "FILE FOUND[%s]=>[%s]"%(single,base))
+
+
 
 
 def SPECIFICWORD(phrase):
@@ -58,7 +104,7 @@ def SPECIFICWORD(phrase):
 
 ###### SHOW THE TITLE ###########
 for menu_line in menu:
-	print menu_line
+	color.err(menu_line)
 
 ##### VALIDATING OPTIONS ########
 while option not in search:
@@ -74,9 +120,7 @@ elif option == "2":
 	phrase = raw_input("FOLDER NAME \n>:")
 	FOLDERS(phrase)
 elif option == "3":
-	ext = raw_input("EXTENTION FILE  \n>:")
-	phrase = raw_input("FILE NAME\n")
-	SPECIFICFILE(ext,phrase)
+	SPECIFICFILES()
 elif option == "4":
 	phrase = raw_input("WORD \n>:")
 	SPECIFICWORD(phrase)
